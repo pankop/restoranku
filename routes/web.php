e36f6ddb3923d07017bf5c2d30df861d34bda702
pankop <zakymuslim7@gmail.com>
2025-09-07 12:03:22 +0700

    feat: add views customer

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/customer', function () {
+    return view('/customer/layouts/master');
+});
