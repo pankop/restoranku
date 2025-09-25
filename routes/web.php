a43712dee6bdd67f8646730b1d2c7b70202e932a
pankop <zakymuslim7@gmail.com>
2025-09-25 08:54:47 +0700

    feat: add templating blade for customer, edit menu view

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/customer', function () {
-    return view('/customer/layouts/master');
+Route::get('/customer/menu', function () {
+    return view('customer.menu');
 });
